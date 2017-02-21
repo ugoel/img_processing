a2c51b4afe5c221a98b0f940dad56fbf8127a160
Utkarsh Goel <utkarsh.090@gmail.com>
2017-02-21 14:31:51 -0700

    new code

--- ASS2/capture.py
+++ ASS2/capture.py
@@ -4,7 +4,10 @@ import numpy as np
 import os
 
 def capture_webcam():
-    camera = cv.VideoCapture(0)   
+    camera = cv.VideoCapture(0)
+    ret_val, tempImg = camera.read()
+    tempImg = cv.resize(tempImg, (320,240))
+    imgAcw = np.zeros((tempImg.shape), np.float32)
     while True:
         ret_val, img = camera.read()
         if ret_val:
@@ -14,6 +17,7 @@ def capture_webcam():
             cv.moveWindow('regular', 0, 0)
             img = cv.resize(img, (320,240))
             cv.imshow('regular', img)
+            imgBlank = img.copy()
 
             cv.namedWindow('grayscale', cv.WINDOW_NORMAL)
             cv.resizeWindow('grayscale', 320,240)
@@ -24,27 +28,31 @@ def capture_webcam():
             cv.namedWindow('capture clone - image 1', cv.WINDOW_NORMAL)
             cv.resizeWindow('capture clone - image 1', 320,240)
             cv.moveWindow('capture clone - image 1', 0, 265)
-            cv.imshow('capture clone - image 1', img)
+            
 
             #brightening the image
             img_yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
             img_yuv[:,:,0] = cv.equalizeHist(img_yuv[:,:,0])
             img_output = cv.cvtColor(img_yuv, cv.COLOR_YUV2BGR)
-            blur = cv.GaussianBlur(img_yuv, (5,5),0)
+            cv.imshow('capture clone - image 1', img_output)
+            blur = cv.GaussianBlur(img_output, (5,5),0)
 
-            imgAcw = np.float32(blur)
-            cv.accumulateWeighted(blur, imgAcw, 0.01)
-            raImg = cv.convertScaleAbs(imgAcw)
-            diff = cv.absdiff(blur, raImg)
-            diffGray =cv.cvtColor(raImg, cv.COLOR_BGR2GRAY)
-            ret,thresh = cv.threshold(diffGray,127,255,0)
-            im2, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-            cv.drawContours(thresh, contours, -1, (0,255,0), 2)
+            cv.accumulateWeighted(blur, imgAcw, 0.32)
+            cv.convertScaleAbs(imgAcw, imgBlank, 1.0, 0.0)
+            diff = cv.absdiff(blur, imgBlank)
+            diffGray =cv.cvtColor(diff, cv.COLOR_BGR2GRAY)
+            
+            ret,thresh = cv.threshold(diffGray,2,255,0)
+            blur1 = cv.GaussianBlur(thresh, (5,5), 1.5)
+            ret,thresh1 = cv.threshold(blur1,250,255,0)
+            im2, contours, hierarchy = cv.findContours(thresh1,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+            
+            cv.drawContours(thresh1, contours, -1, (0,255,0), 2)
             
             cv.namedWindow('new frame', cv.WINDOW_NORMAL)
             cv.resizeWindow('new frame', 320,240)
             cv.moveWindow('new frame', 320, 265)
-            cv.imshow('new frame', thresh)
+            cv.imshow('new frame', thresh1)
 
         if cv.waitKey(1) == 27:
             break
